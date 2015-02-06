2e0645542ae26d5bc3c6518893dbb1d5b61d507e
Marcus Kinsella <mkinsella@pacificbiosciences.com>
2015-02-05 16:09:40 -0800

    Add a -S parameter for output style, OBT or OVL.

--- las_to_ovb.cpp
+++ las_to_ovb.cpp
@@ -2,39 +2,57 @@
 #include "Overlap.h"
 #include "LASReader.h"
 
+#include <cstdlib>
 #include <cstring>
 #include <iostream>
 #include <string>
 
 int main(int argc, char* argv[])
 {
-  
-  std::string las_name, db_name, fastq_name, ovb_name;
+  std::string las_name, ovb_name, ovb_style;
 
   int arg = 1;
   while(arg < argc) {
     if(strcmp(argv[arg], "-L") == 0) {
       las_name = std::string(argv[++arg]);
     }
-    if(strcmp(argv[arg], "-D") == 0) {
-      db_name = std::string(argv[++arg]);
-    }
-    if(strcmp(argv[arg], "-F") == 0) {
-      fastq_name = std::string(argv[++arg]);
-    }
     if(strcmp(argv[arg], "-O") == 0) {
       ovb_name = std::string(argv[++arg]);
+    }
+    if(strcmp(argv[arg], "-S") == 0) {
+      ovb_style = std::string(argv[++arg]);
     }
     arg++;
   }
   
-  LASReader las_reader(las_name, db_name);
-  OVBWriter ovb_writer(ovb_name, fastq_name); 
+  if(las_name.size() == 0 || ovb_name.size() == 0) {
+    std::cerr << "Usage: las_to_ovb -L <las_name> -O <ovb_name> ";
+    std::cerr << "-S <style: obt or ovl>" << std::endl;
+    exit(1);
+  }
+     
+  if(!(ovb_style == "obt" || ovb_style == "ovl")) {
+    std::cerr << "OVB style (-S) must be obt or ovl." << std::endl;
+    exit(1);
+  }
+  
+  OVBWriter* writer_ptr;
+  if(ovb_style == "obt") {
+    writer_ptr = new OBTWriter(ovb_name);
+  } else if(ovb_style == "ovl") {
+    writer_ptr = new OVLWriter(ovb_name);
+  }
+
+  LASReader las_reader(las_name);
   Overlap_T overlap;
 
   while(las_reader.next_overlap(&overlap)) {
-    ovb_writer.write_overlap(overlap);
+    // We don't want to write each overlap twice, so only write those where the
+    // ids are ordered.
+    if(overlap.id_a > overlap.id_b) continue;
+    writer_ptr->write_overlap(overlap);
   }
   
+  delete writer_ptr; 
   return 0;
 }
