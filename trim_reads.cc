5e68a84d27d62d315cf5efb603c75991554915dc
Marcus Kinsella <mkinsella@pacificbiosciences.com>
2015-02-27 15:17:56 -0800

    Parse the coded input differently, using a method that cannot possibly be correct.

--- trim_reads.cc
+++ trim_reads.cc
@@ -14,10 +14,10 @@
 void write_trimmed_read(const proto::Read& read,
                         google::protobuf::io::CodedOutputStream* output)
 {
-  std::cerr << " Read id: " << read.id() << " (" << read.trimmed_start();
-  std::cerr << "-" << read.trimmed_end() << ") " << read.untrimmed_length();
-  if(read.trimmed_end() - read.trimmed_start() != read.untrimmed_length()) std::cerr << " *";
-  std::cerr << std::endl;
+  //std::cerr << " Read id: " << read.id() << " (" << read.trimmed_start();
+  //std::cerr << "-" << read.trimmed_end() << ") " << read.untrimmed_length();
+  //if(read.trimmed_end() - read.trimmed_start() != read.untrimmed_length()) std::cerr << " TRIMMED";
+  //std::cerr << std::endl;
   output->WriteVarint32(read.ByteSize());
   read.SerializeToCodedStream(output);
 }
@@ -33,7 +33,7 @@ int main(int argc, char* argv[])
   std::string overlap_file_name;
   int agglomeration_distance = 100;
   int termination_count_threshold = 3;
-  int max_deception_length = 50;
+  int max_deception_length = 400;
   int min_spanned_coverage = 1;
 
   int arg = 1;
@@ -67,6 +67,7 @@ int main(int argc, char* argv[])
     input_fd = open(overlap_file_name.c_str(), O_RDONLY);
     raw_input = new google::protobuf::io::FileInputStream(input_fd);
   }
+
   auto coded_input = new google::protobuf::io::CodedInputStream(raw_input);
 
   proto::Overlap overlap;
@@ -78,7 +79,7 @@ int main(int argc, char* argv[])
   uint32_t record_size = 0;
 
   int current_id1 = -1;
-
+  int counter = 0;
   while(coded_input->ReadVarint32(&record_size)) {
 
     if(record_size > buffer_size) {
@@ -88,19 +89,24 @@ int main(int argc, char* argv[])
 
     coded_input->ReadRaw(buffer, record_size);
     overlap.ParseFromArray(buffer, record_size);
+    delete coded_input;
+    coded_input = new google::protobuf::io::CodedInputStream(raw_input);
     
     if(overlap.id_1() != current_id1) {
       if(overlaps.size() > 0) {
         trimmed_read = trim_overlaps(&overlaps, agglomeration_distance, termination_count_threshold,
                                      max_deception_length, min_spanned_coverage);
         write_trimmed_read(trimmed_read, coded_read_output);
+        counter++;
+        if(counter % 1000 == 0) std::cerr << "Trimming read " << counter << std::endl;
+        if(counter % 2000 == 0) break;
       }
       overlaps.clear();
       current_id1 = overlap.id_1();
     }
     overlaps.push_back(overlap);
   }
-
+  
   if(overlaps.size() > 0) {
     trimmed_read = trim_overlaps(&overlaps, agglomeration_distance, termination_count_threshold,
                                 max_deception_length, min_spanned_coverage);
